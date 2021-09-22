3858ccd1509d6d2299f6ef0b5d642c72368ce523
NicolasPeruchot <nicolas.peruchot@gmail.com>
2021-09-22 09:59:21 +0200

    Add HF to app

--- Offers.py
+++ Offers.py
@@ -1,6 +1,7 @@
 import streamlit as st
 
 from scrapping.config import countries
+from scrapping.HF import get_job_HF
 from scrapping.the_hub import get_job_hub
 
 FLAGS = {x: "flag-" + x.lower() for x in countries}
@@ -11,15 +12,28 @@ def header() -> None:
     st.title("Where should I apply?")
 
 
-def build_row(job: dict):
-    col1, col2, col3 = st.columns([3, 10, 1])
-    col1.image(job["image"])
-    col2.write(f"[{job['title']}]({job['url']})")
-    col3.write(f":{FLAGS[job['country']]}:")
+def build_hub_rows():
+    jobs = get_job_hub()
+    for job in jobs:
+        col1, col2, col3 = st.columns([3, 7, 1])
+        col1.image(job["image"])
+        col2.write(f"[{job['title']}]({job['url']})")
+        col3.write(f":{FLAGS[job['country']]}:")
+
+
+def build_HF_rows():
+    jobs = get_job_HF()
+    for i, row in jobs.iterrows():
+        col1, col2 = st.columns([5, 1])
+        col1.write(row["Title"])
+        col2.write(row["City"])
 
 
 if __name__ == "__main__":
     header()
-    jobs = get_job_hub()
-    for job in jobs:
-        build_row(job)
+    st.write("## The Hub:")
+    with st.spinner("Loading Hub jobs..."):
+        build_hub_rows()
+    st.write("## Hello Fresh:")
+    with st.spinner("Loading HF jobs..."):
+        build_HF_rows()
--- scrapping/HF.py
+++ scrapping/HF.py
@@ -1,19 +1,20 @@
 import pandas as pd
 from gazpacho import Soup, get
 
-from scrapping.config import matches
+HF_url = "https://www.hellofresh.com.au/careers/team/data-science-business-intelligence-7043"
 
-url = "https://www.hellofresh.com.au/careers/team/data-science-business-intelligence-7043"
 
-html = get(url)
-soup = Soup(html)
+def get_job_HF():
 
-titles = []
-places = []
-for job in soup.find("div", {"class": "fela-_1y1x7xu"}):
-    title = job.find("a", {"class": "fela-_eumwml"}).text
-    if any(x in title for x in matches):
-        titles.append(title)
-        places.append(job.find("div", {"class": "fela-_17b356z"}).text)
+    html = get(HF_url)
+    soup = Soup(html)
 
-print(pd.DataFrame({"Title": titles, "City": places}))
+    titles = []
+    places = []
+    for job in soup.find("div", {"class": "fela-_1y1x7xu"}):
+        title = job.find("a", {"class": "fela-_eumwml"}).text
+        if "Intern" in title:
+            titles.append(title)
+            places.append(job.find("div", {"class": "fela-_17b356z"}).text)
+
+    return pd.DataFrame({"Title": titles, "City": places})
